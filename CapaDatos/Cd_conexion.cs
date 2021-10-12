daf7815097b40480fb8fd68e6480d2612d2e3f5c
bryan <bryan@BRYAN>
2021-10-12 14:37:36 -0500

    Agregar archivos de proyecto.

---
+++ CapaDatos/Cd_conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+namespace CapaDatos
+{
+    public class Cd_conexion
+    {
+        SqlConnection cn = new SqlConnection(@"Data Source=BRYAN;Initial Catalog=RRHHECU911;Integrated Security=True");
+        
+    }
+}
