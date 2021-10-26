9ca1531be1c91b57c641e7bb572fb422b9b81f87
William Simbaña <andre@DESKTOP-GH8860H>
2021-10-26 17:47:03 -0500

    cambios cargo

--- RRHHECU911/Vistas/data/cargo.aspx.cs
+++ RRHHECU911/Vistas/data/cargo.aspx.cs
@@ -17,37 +17,9 @@ namespace RRHHECU911.vistas.data
             ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Datos Guardados')", true);
         }
 
-        //protected void Btn_Guardar_Click(object sender, EventArgs e)
-        //{
-        //    Registrar_Cargoinstitucional();
-        //}
-
-
-
-        public void Registrar_Cargoinstitucional()
+        protected void Button1_Click(object sender, EventArgs e)
         {
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Asegurese que los campos esten llenos!!')", true);
-            if (string.IsNullOrEmpty(Txt_Nombrecargo.Text) || String.IsNullOrEmpty(Txt_Estadocargo.Text))
-            {
-                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Asegurese que los campos esten llenos!!')", true);
-            }
-            else
-            {
-                dc.Registro_CargoInstitucional(Txt_Nombrecargo.Text, Txt_Estadocargo.Text);
-                Limpiar();
-                ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Datos Guardados')", true);
-            }
-        }
-
-        private void Limpiar()
-        {
-            Txt_Nombrecargo.Text = Txt_Estadocargo.Text = "";
-        }
-
-        protected void Btn_Guardar_Click1(object sender, EventArgs e)
-        {
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Asegurese que los campos esten llenos!!')", true);
-
+            //Message.Text = "Hello World!!";
         }
     }
 }
--- RRHHECU911/Vistas/data/cargo.aspx.designer.cs
+++ RRHHECU911/Vistas/data/cargo.aspx.designer.cs
@@ -15,12 +15,39 @@ namespace RRHHECU911.vistas.data
     {
 
         /// <summary>
-        /// Control Button1.
+        /// Control Txt_Nombrecargo.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.TextBox Txt_Nombrecargo;
+
+        /// <summary>
+        /// Control Txt_Estadocargo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Estadocargo;
+
+        /// <summary>
+        /// Control Btn_Guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Guardar;
+
+        /// <summary>
+        /// Control Btn_Cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Cancelar;
     }
 }
