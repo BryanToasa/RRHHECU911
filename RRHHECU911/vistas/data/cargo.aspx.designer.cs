5039bea26501fde56a457df1fd441ac58b31afef
William Simbaña <andre@DESKTOP-GH8860H>
2021-10-26 17:02:25 -0500

    Cambios boton Cargo2

--- RRHHECU911/vistas/data/cargo.aspx.designer.cs
+++ RRHHECU911/vistas/data/cargo.aspx.designer.cs
@@ -13,5 +13,41 @@ namespace RRHHECU911.vistas.data
 
     public partial class cargo
     {
+
+        /// <summary>
+        /// Control Txt_Nombrecargo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Nombrecargo;
+
+        /// <summary>
+        /// Control Txt_Estadocargo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Estadocargo;
+
+        /// <summary>
+        /// Control Btn_Guardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Guardar;
+
+        /// <summary>
+        /// Control Btn_Cancelar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Btn_Cancelar;
     }
 }
